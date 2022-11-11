faf518ca5d9cfef3d1553b49b296cbb9f057f21b
Hussain Rezai <hussain.rasa111@gmail.com>
2022-11-11 16:46:41 -0500

    "third commit from eclipse"

--- src/testingEclispse/TestClass.java
+++ src/testingEclispse/TestClass.java
@@ -5,5 +5,11 @@ public class TestClass {
 	public static void main(String[] args) {
 		System.out.println("this is just a test!");
 		System.out.println("this is the second test message");
+		
+		
+	}
+	
+	public static void test() {
+		System.out.println("just a public static void method");
 	}
 }
