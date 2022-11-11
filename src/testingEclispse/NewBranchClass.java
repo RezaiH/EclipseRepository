d91496bcb508e49771c4626e88d1fd82d1135d96
Hussain Rezai <hussain.rasa111@gmail.com>
2022-11-11 17:58:36 -0500

    second commit

--- src/testingEclispse/NewBranchClass.java
+++ src/testingEclispse/NewBranchClass.java
@@ -4,5 +4,11 @@ public class NewBranchClass {
 
 	public static void main(String[] args) {
 		System.out.println("Hi, how are you doing?");
+		secondBranch();
+	}
+	public static void secondBranch() {
+		String name = "Mudaser Jan";
+		int age = 10;
+		System.out.println(name + " is " + age + " years old.");
 	}
 }
